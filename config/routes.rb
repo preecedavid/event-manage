71e0f8e573466edf9ce1edc44df67d66a0c5acdf
preecedavid <davidpreece.upwork@gmail.com>
2021-05-10 16:20:22 +0300

    don't use active admin devise part

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+
+  resources :home, only: :index
+
   root to: redirect('/admin')
   # mount Sidekiq::Web => '/sidekiq'
 end
