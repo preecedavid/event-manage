e1a4a3922e315d7e38d2120ef880aaa779b715d7
preecedavid <davidpreece.upwork@gmail.com>
2021-05-09 22:01:51 -0400

    Remove public routes and make root Active Admin for pen test, Update rails to address vulnerability, Use TLS url for redis, Disable SSL certificate verification, Disable sidekiq

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.2'
 
-gem 'rails', '~> 6.1.3', '>= 6.1.3.1'
+gem 'rails', '~> 6.1.3', '>= 6.1.3.2'
 gem 'pg', '~> 1.1'
 
 # Use Puma as the app server
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
-require 'connection_pool'
-
-$redis = ConnectionPool::Wrapper.new(size: 10, timeout: 2) { Redis.new(url: ENV['REDIS_URL']) }
-
-Sidekiq.configure_server do |config|
-  config.options = config.options.merge(queues: %w( default active_storage_analysis ), concurrency: ENV.fetch("WORKER_MAX_THREADS") { 5 })
-  config.default_worker_options = config.default_worker_options.merge(retry: false)
-  config.redis = $redis
-end if defined?(Sidekiq)
-
-Sidekiq.configure_client do |config|
-  config.default_worker_options = config.default_worker_options.merge(retry: false)
-  config.redis = $redis
-end if defined?(Sidekiq)
+#require 'connection_pool'
+#
+#$redis = ConnectionPool::Wrapper.new(size: 10, timeout: 2) { Redis.new(url: ENV['REDIS_TLS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } ) }
+#
+#Sidekiq.configure_server do |config|
+#  config.options = config.options.merge(queues: %w( default active_storage_analysis ), concurrency: ENV.fetch("WORKER_MAX_THREADS") { 5 })
+#  config.default_worker_options = config.default_worker_options.merge(retry: false)
+#  config.redis = $redis
+#end if defined?(Sidekiq)
+#
+#Sidekiq.configure_client do |config|
+#  config.default_worker_options = config.default_worker_options.merge(retry: false)
+#  config.redis = $redis
+#end if defined?(Sidekiq)
