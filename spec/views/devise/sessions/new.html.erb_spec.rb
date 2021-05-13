f4cd3d4923fa47d1e02192663488f6adbd81ab9e
preecedavid <davidpreece.upwork@gmail.com>
2021-05-13 18:02:07 -0400

    Disable autocomplete for login form

---
+++ spec/views/devise/sessions/new.html.erb_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+
+RSpec.describe "devise/sessions/new", type: :view do
+
+  module DeviseUserBits
+    def resource
+      User.new
+    end
+
+    def resource_name
+      :user
+    end
+
+    def devise_mapping
+      Devise.mappings[:user]
+    end
+  end
+
+  before(:each) do
+    view.class.include DeviseUserBits
+  end
+
+  it "renders new event form" do
+    render
+
+    assert_select "form[autocomplete=off]"
+  end
+end
