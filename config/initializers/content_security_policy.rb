e23144359900f7012cfdc40592ad4d4b37688f2b
preecedavid <davidpreece.upwork@gmail.com>
2021-05-13 18:06:54 -0400

    Constrain CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,11 +6,11 @@
 
 Rails.application.config.content_security_policy do |policy|
   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
+   policy.font_src    :self, :https, :data
+   policy.img_src     :self, :https, :data
+   policy.object_src  :none
+   policy.script_src  :self, :https
+   policy.style_src   :self, :https
 #   # If you are using webpack-dev-server then specify webpack-dev-server host
 #   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
 
