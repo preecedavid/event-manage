bdae02f60351b2cfa2b446d7397de57a4f88e5f5
preecedavid <davidpreece.upwork@gmail.com>
2021-05-13 18:05:40 -0400

    Remove reference to sidekiq in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   config.force_ssl = true
   config.ssl_options = { hsts: { expires: 10.days, preload: true } }
-  config.active_job.queue_adapter = :sidekiq
   config.action_controller.asset_host = ENV['CLOUDFRONT_URL']
   config.cache_store = :redis_cache_store, { url: ENV['REDIS_CACHE_URL'] }
 
