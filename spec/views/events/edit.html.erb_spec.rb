05964d390bba9adb8166fb9b13976bbea5b346ea
preecedavid <davidpreece.upwork@gmail.com>
2021-05-19 20:46:24 +0000

    Fix falling tests

--- spec/views/events/edit.html.erb_spec.rb
+++ spec/views/events/edit.html.erb_spec.rb
@@ -4,13 +4,14 @@ require 'rails_helper'
 
 RSpec.describe 'events/edit', type: :view do
   before do
-    @event = assign(:event, create(:event))
+    event = assign(:event, create(:event))
+    assign(:attendees, event.attendees)
   end
 
   it 'renders the edit event form' do
     render
 
-    assert_select 'form[action=?][method=?]', event_path(@event), 'post' do
+    assert_select 'form[action=?][method=?]', event_path(Event.last), 'post' do
     end
   end
 end
--- spec/views/events/show.html.erb_spec.rb
+++ spec/views/events/show.html.erb_spec.rb
@@ -4,8 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'events/show', type: :view do
   before do
-    event = assign(:event, create(:event))
-    assign(:attendees, event.attendees)
+    assign(:event, create(:event))
   end
 
   it 'renders attributes in <p>' do
