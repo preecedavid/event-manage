e8d09af00ed1a50a16b0e344d048e4789c88eef3
preecedavid <davidpreece.upwork@gmail.com>
2021-05-20 17:15:57 -0400

    Add roles to user admin page, Add first name and last name to user AA

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation
+  permit_params :first_name, :last_name, :email, :password, :password_confirmation, role_ids: []
 
   index do
     selectable_column
     id_column
+    column :first_name
+    column :last_name
     column :email
     column :current_sign_in_at
     column :sign_in_count
@@ -20,10 +22,24 @@ ActiveAdmin.register User do
 
   form do |f|
     f.inputs do
+      f.input :first_name
+      f.input :last_name
       f.input :email
       f.input :password
       f.input :password_confirmation
+      f.input :roles, as: :check_boxes
     end
     f.actions
+  end
+
+  before_action :remove_password_params_if_blank, only: [:update]
+
+  controller do
+    def remove_password_params_if_blank
+      return if params[:user][:password].blank? || params[:user][:password_confirmation].blank?
+
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
   end
 end
