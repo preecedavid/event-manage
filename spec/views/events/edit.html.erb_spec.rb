c64fca4ac9d9aa8a1d6b8041251e2b383d8fd462
preecedavid <davidpreece.upwork@gmail.com>
2021-05-28 01:21:34 -0400

    Fix event edit spec

--- spec/views/events/edit.html.erb_spec.rb
+++ spec/views/events/edit.html.erb_spec.rb
@@ -3,8 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe 'events/edit', type: :view do
+  let(:event) { create(:event) }
+  let(:rooms) { create_list(:room, 2) }
+
   before do
-    event = assign(:event, create(:event))
+    assign(:rooms, rooms)
+    assign(:event, event)
     assign(:attendees, event.attendees)
   end
 
