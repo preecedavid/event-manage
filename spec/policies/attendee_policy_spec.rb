06e0741213361b081b81ed3aa85a68baa0456337
preecedavid <davidpreece.upwork@gmail.com>
2021-05-31 15:43:57 +0000

    Fix tests

--- spec/policies/attendee_policy_spec.rb
+++ spec/policies/attendee_policy_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe AttendeesPolicy, type: :policy do
+RSpec.describe AttendeePolicy, type: :policy do
   subject { described_class }
 
   it { is_expected.to be < AdministratorPolicy }
