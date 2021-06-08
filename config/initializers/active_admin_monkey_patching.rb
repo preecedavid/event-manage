b50fb70e9b4ec61cbc939e92a8bc411beb9cff69
preecedavid <davidpreece.upwork@gmail.com>
2021-06-08 22:37:24 +0000

    Fix errors related with models using FriendlyId in ActiveAdmin dashboard

---
+++ config/initializers/active_admin_monkey_patching.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+ActiveAdmin::ResourceController.class_eval do
+  def find_resource
+    finder = resource_class.is_a?(FriendlyId) ? :slug : :id
+    scoped_collection.find_by(finder => params[:id])
+  end
+end
