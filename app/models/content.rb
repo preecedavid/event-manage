882317498e1787193a4dcaca490b6718c8ee2be9
preecedavid <davidpreece.upwork@gmail.com>
2021-06-09 19:46:30 +0000

    Allow upload of several mime types

--- app/models/content.rb
+++ app/models/content.rb
@@ -10,9 +10,25 @@
 #  updated_at :datetime         not null
 #
 class Content < ApplicationRecord
+  ALLOWED_MIME_TYPES = %w[
+    application/pdf
+    image/jpeg
+    image/png
+    image/webp
+    video/mp4
+    image/gif
+    image/gif
+    application/vnd.ms-powerpoint
+    application/vnd.openxmlformats-officedocument.presentationml.presentation
+  ].freeze
+
   acts_as_taggable_on :tags
 
   has_one_attached :file
 
   validates :name, presence: true
+
+  def self.mime_types
+    ALLOWED_MIME_TYPES.join(', ')
+  end
 end
