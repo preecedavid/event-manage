c10f43908d3252f66e80564cf8f0497962c7a89a
preecedavid <davidpreece.upwork@gmail.com>
2021-06-09 17:48:19 -0400

    Delete related hotspots when content is deleted

--- app/models/content.rb
+++ app/models/content.rb
@@ -24,6 +24,7 @@ class Content < ApplicationRecord
 
   acts_as_taggable_on :tags
 
+  has_many :hotspots, dependent: :destroy
   has_one_attached :file
 
   validates :name, presence: true
--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -12,5 +12,5 @@
 require 'rails_helper'
 
 RSpec.describe Content, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_many(:hotspots).dependent(:destroy) }
 end
