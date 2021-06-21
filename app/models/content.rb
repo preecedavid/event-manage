f539523dcae134526976431f4f6863f68e2ea822
preecedavid <davidpreece.upwork@gmail.com>
2021-06-21 17:22:47 -0400

    Content file size limits

--- app/models/content.rb
+++ app/models/content.rb
@@ -28,6 +28,7 @@ class Content < ApplicationRecord
   has_one_attached :file
 
   validates :name, presence: true
+  validate :acceptable_file
 
   delegate :key, to: :file, allow_nil: false, prefix: true
 
@@ -35,5 +36,23 @@ class Content < ApplicationRecord
 
   def self.mime_types
     ALLOWED_MIME_TYPES.join(', ')
+  end
+
+  def acceptable_file
+    return unless file.attached?
+
+    document_types = [
+      'application/vnd.ms-powerpoint',
+      'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+    ]
+
+    max_file_size = if document_types.include?(file.content_type)
+                      # only document types
+                      25.megabyte
+                    else
+                      50.megabyte
+                    end
+
+    errors.add(:file, 'is too big') if file.byte_size >= max_file_size
   end
 end
