036a0dcc13a31d161f804eabbe02297ab3358eb2
preecedavid <davidpreece.upwork@gmail.com>
2021-06-20 20:28:37 -0400

    liveserach added to Event Hotspot content select

--- app/javascript/controllers/select_controller.js
+++ app/javascript/controllers/select_controller.js
@@ -17,10 +17,38 @@ export default class extends Controller {
           },
           processResults: function (data) {
             let formatedData = $.map(data, function (obj) {
-              obj.id = obj.file_blob.key;
-              obj.text = obj.name;
+              let newObj = {};
+              newObj.id = obj.file_blob.key;
+              newObj.text = obj.name;
             
-              return obj;
+              return newObj;
+            });
+
+            return {
+              results: formatedData
+            };
+          }
+        }
+      })
+    } else if (this.isHotspotEventContentSelect) {
+      $(this.element).select2({
+        dropdownParent: $('#bindContentModal'), // https://select2.org/troubleshooting/common-problems
+        ajax: {
+          url: '/contents.json',
+          dataType: 'json',
+          data: function (params) {
+            var query = {
+              'q[name_cont]': params.term,
+            }
+            return query;
+          },
+          processResults: function (data) {
+            let formatedData = $.map(data, function (obj) {
+              let newObj = {};
+              newObj.id = obj.id;
+              newObj.text = obj.name;
+            
+              return newObj;
             });
 
             return {
@@ -40,6 +68,10 @@ export default class extends Controller {
 
   get isEventContentSelect() {
     return this.element.dataset.type && this.element.dataset.type === 'event-content-select';
+  }
+
+  get isHotspotEventContentSelect() {
+    return this.element.dataset.type && this.element.dataset.type === 'event-hotspot-content-select';
   }
 
 }
