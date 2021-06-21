f629de2df352ac3ac09e75974301e1e1b8166ae1
preecedavid <davidpreece.upwork@gmail.com>
2021-06-20 20:58:26 -0400

    code optimized

--- app/javascript/controllers/select_controller.js
+++ app/javascript/controllers/select_controller.js
@@ -4,59 +4,9 @@ export default class extends Controller {
 
   connect() {
     if (this.isEventContentSelect) {
-      $(this.element).select2({
-        ajax: {
-          url: '/contents.json',
-          dataType: 'json',
-          data: function (params) {
-            var query = {
-              'q[name_cont]': params.term,
-              type: 'images'
-            }
-            return query;
-          },
-          processResults: function (data) {
-            let formatedData = $.map(data, function (obj) {
-              let newObj = {};
-              newObj.id = obj.file_blob.key;
-              newObj.text = obj.name;
-            
-              return newObj;
-            });
-
-            return {
-              results: formatedData
-            };
-          }
-        }
-      })
+      this.initContentSelect()
     } else if (this.isHotspotEventContentSelect) {
-      $(this.element).select2({
-        dropdownParent: $('#bindContentModal'), // https://select2.org/troubleshooting/common-problems
-        ajax: {
-          url: '/contents.json',
-          dataType: 'json',
-          data: function (params) {
-            var query = {
-              'q[name_cont]': params.term,
-            }
-            return query;
-          },
-          processResults: function (data) {
-            let formatedData = $.map(data, function (obj) {
-              let newObj = {};
-              newObj.id = obj.id;
-              newObj.text = obj.name;
-            
-              return newObj;
-            });
-
-            return {
-              results: formatedData
-            };
-          }
-        }
-      })
+      this.initContentSelect(true)
     } else {
       $(this.element).select2()
     }
@@ -64,6 +14,46 @@ export default class extends Controller {
 
   disconnect() {
     $(this.element).select2("destroy")
+  }
+
+  initContentSelect(hotspotAttachSelect = false) {
+    let options = {
+      ajax: {
+        url: '/contents.json',
+        dataType: 'json',
+        data: function (params) {
+          var query = {
+            'q[name_cont]': params.term,
+          }
+          return query;
+        },
+        processResults: function (data) {
+          let formatedData = $.map(data, function (obj) {
+            let newObj = {};
+            newObj.text = obj.name;
+            if (hotspotAttachSelect) {
+              newObj.id = obj.id;
+            } else {
+              newObj.id = obj.file_blob.key;
+            }
+          
+            return newObj;
+          });
+
+          return {
+            results: formatedData
+          };
+        }
+      }
+    }
+
+    if (hotspotAttachSelect) {
+      // Select2 does not function properly when I use it inside a Bootstrap modal
+      // https://select2.org/troubleshooting/common-problems
+      options.dropdownParent = $('#bindContentModal')
+    }
+
+    $(this.element).select2(options)
   }
 
   get isEventContentSelect() {
